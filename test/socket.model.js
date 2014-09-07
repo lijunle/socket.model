a0b4241052f0b12c021f2cf69e7338f51404da75
Junle <lijunle@gmail.com>
2014-09-08 03:20:25 +0800

    Implement two feature about connect event.
    - Trigger connect event on connected socket when new model.
    - Respone connect data to client in server side connect handler.
    - Refactor the test to make it clearer.

--- test/socket.model.js
+++ test/socket.model.js
@@ -10,18 +10,58 @@ var SocketModel = require('../lib/socket.model');
 
 describe('SocketModel', function () {
 
-  it('should trigger connect event when a client connects', function (done) {
-    var srv = http();
-    var sio = io(srv);
-    var Post = new SocketModel('post', sio);
+  var sio;
+  var port;
 
+  beforeEach(function (done) {
+    var srv = http();
+    sio = io(srv);
+
+    srv.listen(function () {
+      port = srv.address().port;
+      done();
+    });
+  });
+
+  function createClient() {
+    return ioc('ws://127.0.0.1:' + port);
+  }
+
+  it('should trigger connect event when a client connects', function (done) {
+    var Post = new SocketModel('post', sio);
     Post.on('connect', function () {
       expect(true).to.be.ok();
       done();
     });
 
-    srv.listen(function () {
-      ioc('ws://127.0.0.1:' + srv.address().port);
+    createClient();
+  });
+
+  it('should trigger connect event on the connected client', function (done) {
+    var client = createClient();
+    client.on('connect', function () {
+
+      // new socket model after client is connected
+      var Post = new SocketModel('post', sio);
+      Post.on('connect', function () {
+        expect(true).to.be.ok();
+        done();
+      });
+    });
+  });
+
+  it('should trigger client connect event when response', function (done) {
+    var expectedPosts = ['p1', 'p2', 'p3'];
+
+    var Post = new SocketModel('post', sio);
+    Post.on('connect', function (res) {
+      res(expectedPosts);
+    });
+
+    var client = createClient();
+    client.on('post:connect', function (actualPosts) {
+      expect(actualPosts).to.eql(expectedPosts);
+      done();
     });
   });
 
