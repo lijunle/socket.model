1581adef841c8fedd75045a1810db565f1c2d298
Junle <lijunle@gmail.com>
2014-09-08 23:35:22 +0800

    Implement non-connect event handlers.
    - Add unit test.
    - Reference lodash as dependency.
    - Reference Q as dev-dependency.

--- test/socket.model.js
+++ test/socket.model.js
@@ -7,25 +7,26 @@ var io = require('socket.io');
 var ioc = require('socket.io-client');
 var expect = require('expect.js');
 var SocketModel = require('../lib/socket.model');
+var Q = require('q');
 
-describe('SocketModel', function () {
+var sio;
+var port;
 
-  var sio;
-  var port;
+beforeEach(function (done) {
+  var srv = http();
+  sio = io(srv);
 
-  beforeEach(function (done) {
-    var srv = http();
-    sio = io(srv);
-
-    srv.listen(function () {
-      port = srv.address().port;
-      done();
-    });
+  srv.listen(function () {
+    port = srv.address().port;
+    done();
   });
+});
 
-  function createClient() {
-    return ioc('ws://127.0.0.1:' + port);
-  }
+function createClient() {
+  return ioc('ws://127.0.0.1:' + port);
+}
+
+describe('connect', function () {
 
   it('should trigger connect event when a client connects', function (done) {
     var Post = new SocketModel('post', sio);
@@ -61,6 +62,93 @@ describe('SocketModel', function () {
     var client = createClient();
     client.on('post:connect', function (actualPosts) {
       expect(actualPosts).to.eql(expectedPosts);
+      done();
+    });
+  });
+
+});
+
+describe('create', function () {
+
+  it('should not trigger any event handler when client connect', function (done) {
+    var Post = new SocketModel('post', sio);
+    Post.on('create', function () {
+      expect().fail('');
+    });
+
+    var client = createClient();
+    client.on('connect', function () {
+      setTimeout(done, 500);
+    });
+  });
+
+  it('should trigger create handler when client emit event', function (done) {
+    var Post = new SocketModel('post', sio);
+    Post.on('create', function () {
+      expect(true).to.be.ok();
+      done();
+    });
+
+    var client = createClient();
+    client.emit('post:create');
+  });
+
+  it('should register event handler on connected sockets', function (done) {
+    var client = createClient();
+    client.on('connect', function () {
+      var Post = new SocketModel('post', sio);
+      Post.on('create', function () {
+        expect(true).to.be.ok();
+        done();
+      });
+
+      client.emit('post:create');
+    });
+  });
+
+  var expectedPost = {
+    id: 1234,
+    content: 'post content'
+  };
+
+  it('should communicate between server and client via callback', function (done) {
+    var Post = new SocketModel('post', sio);
+    Post.on('create', function (actualPost, res) {
+      expect(actualPost).to.eql(expectedPost);
+      res(true);
+    });
+
+    var res = function (result) {
+      expect(result).to.be(true);
+      done();
+    };
+
+    var client = createClient();
+    client.emit('post:create', expectedPost, res);
+  });
+
+  it('should broadcast every socket that something is created', function (done) {
+    var Post = new SocketModel('post', sio);
+    Post.on('create', function (actualPost) {
+      expect(actualPost).to.eql(expectedPost);
+    });
+
+    var client1 = createClient();
+    var defer1 = Q.defer();
+    client1.on('post:create', function (actualPost) {
+      expect(actualPost).to.eql(expectedPost);
+      defer1.resolve();
+    });
+
+    var client2 = createClient();
+    var defer2 = Q.defer();
+    client2.on('post:create', function (actualPost) {
+      expect(actualPost).to.eql(expectedPost);
+      defer2.resolve();
+    });
+
+    client2.emit('post:create', expectedPost);
+    Q.all([defer1.promise, defer2.promise]).then(function () {
       done();
     });
   });
